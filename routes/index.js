87c0cd3a915d0faa48d7388ee412ece58909d9f4
Daniel Marmelo <daniel.marmelo@hotmail.com>
2020-05-22 22:55:58 +0100

    It works, Yey!

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/external-api', function(req, res, next) {
+  res.render('external-api');
+});
+
 module.exports = router;
