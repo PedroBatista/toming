bcce6753f30b93662ae165bb903e31ec882fbeb2
Daniel Marmelo <daniel.marmelo@hotmail.com>
2020-06-18 00:36:15 +0100

    changed things...

--- backend/src/routes/rooms.js
+++ backend/src/routes/rooms.js
@@ -1,89 +1,55 @@
 const express = require('express');
 const httpStatus = require('http-status');
-const {User, Session, Room} = require('../database/models');
+const {Room} = require('../database/models');
 const validate = require('../middleware/validate');
 const roomValidation = require('../validations/room.validation');
 const catchAsync = require('../utils/catchAsync');
 const ApiError = require('../utils/ApiError');
-const  isValidSession =  require('../middleware/isValidSession');
-const mongoose = require('mongoose');
+const isValidSession = require('../middleware/isValidSession');
 const jitsiToken = require('../services/jiti-token-generation');
 
 const router = express.Router();
 
-
 router.post('/create',
-  isValidSession,  validate(roomValidation.create),
+  isValidSession,
+  validate(roomValidation.create),
   catchAsync(async (req, res) => {
-      const roomBody = req.body;
+    const roomBody = req.body;
+    roomBody.author = req.session.user;
 
-      try {
-
-        if(!mongoose.Types.ObjectId.isValid(roomBody.author)){
-          return res.status(httpStatus.BAD_REQUEST).send("invalid author id");
-        }
-
-        if ((await User.exists({_id: roomBody.author})) == false) {
-          return res.status(httpStatus.BAD_REQUEST).send("author not found");
-        }
-
-        //ToDo respond in decent api form
-        if ((await Room.exists({subject: roomBody.subject}))) {
-          return res.status(httpStatus.BAD_REQUEST).send("room subject already used");
-        }
-
-        const room = await Room.create(roomBody);
-
-        res.status(httpStatus.CREATED).send(room);
-
-      }catch (e) {
-        console.log("error: " + e)
-        res.status(500).send("internal error");
-      }
-
+    // TODO We should't care! (jitsi uses the mongo id)
+    if (await Room.exists({subject: roomBody.subject})) {
+      throw new ApiError(httpStatus.CONFLICT, "Room subject already used.");
     }
-  )
+
+    const room = await Room.create(roomBody);
+
+    return res.status(httpStatus.CREATED).send(room);
+  })
 );
 
-//list all rooms
-router.get('/',isValidSession,
+// List all user rooms.
+router.get('/',
+  isValidSession,
   catchAsync(async (req, res) => {
-
-      await Room.find().exec()
-        .then((rooms) => {
-          res.status(201).json(rooms)
-        })
-        .catch(err => {
-          res.status(500).json({err: err})
-        })
-
-    }
-  )
+    const rooms = await Room.find({author: req.session.user}).exec();
+    return res.status(httpStatus.CREATED).json(rooms);
+  })
 );
 
 router.get('/:id',
   isValidSession,
   catchAsync(async (req, res) => {
 
-
-    if ((await Room.exists({_id: req.params.id})) == false) {
-      return res.status(httpStatus.BAD_REQUEST).send("room not found");
+    if (await Room.exists({_id: req.params.id}) === false) {
+      throw new ApiError(httpStatus.NOT_FOUND, "Room not found!");
     }
 
-    await Room.findById(req.params.id).populate("author").lean().exec()
-      .then((room) => {
+    const room = await Room.findById(req.params.id).lean().exec();
+    room.jwt_token = jitsiToken.generate(req.session.name, room._id);
 
-        room.jwt_token = jitsiToken.generate(room.author.username, "*");
-        room.author.password ="";
-
-        res.status(200).json(room)
-      })
-      .catch(err => {
-        res.status(500).json({err: err})
-      })
-    }
-  )
+    return res.status(httpStatus.OK).json(room);
+  })
 );
-
 
 module.exports = router;
--- backend/src/validations/room.validation.js
+++ backend/src/validations/room.validation.js
@@ -1,10 +1,8 @@
 const Joi = require('@hapi/joi');
 
-
 const create = {
   body: Joi.object().keys({
-    subject: Joi.string().required(),
-    author: Joi.string().required(),
+    subject: Joi.string().required()
   })
 };
 
