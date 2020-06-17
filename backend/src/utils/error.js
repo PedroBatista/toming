716eb10a76c0908a621433f96e32e038d6db02b6
Daniel Marmelo <daniel.marmelo@hotmail.com>
2020-06-18 00:30:21 +0100

    Better error handling.

--- backend/src/utils/error.js
+++ backend/src/utils/error.js
@@ -1,10 +1,23 @@
+const ApiError = require('../utils/ApiError');
+const httpStatus = require('http-status');
+
 const handleError = (err, res) => {
-  const {statusCode, message} = err;
-  res.status(statusCode).json({
-    status: "error",
-    statusCode,
-    message
-  });
+  if (err instanceof ApiError) {
+    const {statusCode, message} = err;
+    res.status(statusCode).json({
+      status: "error",
+      statusCode,
+      message
+    });
+  }
+  else {
+    console.log(err.stack);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      status: "error",
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      message: err.message
+    });
+  }
 };
 
 module.exports = handleError;
