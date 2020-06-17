d47bbd2eac6207664ea49b84d4b007bbd3029761
Daniel Marmelo <daniel.marmelo@hotmail.com>
2020-06-18 00:27:22 +0100

    Session validator now puts the session on the request. req.session to access it.

--- backend/src/middleware/isValidSession.js
+++ backend/src/middleware/isValidSession.js
@@ -1,27 +1,20 @@
-
 const {Session} = require("../database/models");
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 
+const isValidSession = catchAsync(async (req, res, next) => {
+  console.log("checking session...")
 
-const isValidSession = catchAsync(async (req,res,next)  => {
-  console.log("checking session.. " + req.session)
+  const sess = await Session.findById(req.cookies.sessionid).populate("user", ["name"]).exec();
+  if (sess == null) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, "User session is invalid!");
+  }
 
-  var sess = await Session.findById(req.cookies.sessionid).exec().then(sess => {
-
-      if (sess == null) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, "");
-      }
-
-    }
-  );
+  // Save session on request.
+  req.session = sess;
 
   next();
-
 })
-
-
-
 
 module.exports = isValidSession;
