96b9fb901e2593e1ab8dea1f4440758a63032646
José Feiteira <josefeiteira@gmail.com>
2020-06-20 11:10:22 +0100

    poll.js e poll.validation.js

--- backend/src/validations/poll.validation.js
+++ backend/src/validations/poll.validation.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 
 const create = {
   body: Joi.object().keys({
-    option: Joi.string().required(),
+    question: Joi.string().required(),
     options: Joi.array().min(1).items(Joi.object().keys({
       option: Joi.string().required()
     })).required()
