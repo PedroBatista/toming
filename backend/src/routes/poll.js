d8606804b128b6a58a66fa965dddd25ba37036f5
Daniel Marmelo <daniel.marmelo@hotmail.com>
2020-07-08 22:10:06 +0100

    Polls are now automatically updating the results using SSE.

--- backend/src/routes/poll.js
+++ backend/src/routes/poll.js
@@ -79,14 +79,79 @@ router.get('/:id/vote/:optionId',
     option.vote_count = option.vote_count == undefined ? 1 : ++option.vote_count;
 
     await poll.save();
-
     const pollObj = poll.toObject();
-    pollObj.already_voted = true;
 
+    sendPollUpdate(pollObj);
+
+    pollObj.already_voted = true;
     return res.status(httpStatus.OK).json(pollObj);
   })
 );
 
 const hasAlreadyVoted = async (poll, user) => await Poll.exists({_id: poll._id, already_voted_users: user._id});
+
+let clients = [];
+
+router.get('/:id/events',
+  catchAsync(async (req, res) => {
+
+    // Check if the pool exists
+    if (await Poll.exists({_id: req.params.id}) === false) {
+      throw new ApiError(httpStatus.NOT_FOUND, "Poll not found!");
+    }
+    const poll = await Poll.findById(req.params.id).lean().exec();
+
+    // Mandatory headers and http status to keep connection open
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive',
+      'Cache-Control': 'no-cache'
+    };
+    res.writeHead(200, headers);
+
+    // Tell the client to retry every 10 seconds if connectivity is lost
+    res.write('retry: 10000\n\n');
+
+    // Generate an id based on timestamp and save res object of client connection on clients list
+    // Later we'll iterate it and send updates to each client
+    const clientId = Date.now();
+    const newClient = {
+      id: clientId,
+      pollId: poll._id,
+      res
+    };
+    clients.push(newClient);
+
+    // Keep SSE alive.
+    let keepAliveMS = 60 * 1000;
+
+    function keepAlive() {
+      // SSE comment for keep alive. Chrome times out after two minutes.
+      res.write(':\n\n');
+      setTimeout(keepAlive, keepAliveMS);
+    }
+
+    setTimeout(keepAlive, keepAliveMS);
+
+    /* Event handlers for SSE here */
+
+    // When client closes connection we update the clients list avoiding the disconnected one
+    req.on('close', () => {
+      console.log(`${clientId} Connection closed`);
+      clients = clients.filter(c => c.id !== clientId);
+    });
+  })
+);
+
+// Iterate clients list and use write res object method to send new nest
+const sendPollUpdate = (poll) => {
+  const pollJson = JSON.stringify(poll);
+  clients
+    .filter(c => c.pollId.equals(poll._id))
+    .forEach(c => {
+      c.res.write(`event: poll-update\n`);
+      c.res.write(`data: ${pollJson}\n\n`);
+    })
+};
 
 module.exports = router;
--- frontend/src/views/PollView.vue
+++ frontend/src/views/PollView.vue
@@ -30,13 +30,28 @@
     data() {
       return {
         poll: undefined,
+        eventSource: undefined
       };
     },
     async created() {
       const response = await ApiService.get("/polls/" + this.id)
       this.poll = response.data
+
+      this.setupStream()
+    },
+    beforeDestroy() {
+      this.eventSource.close()
     },
     methods: {
+      setupStream() {
+        // Not a real URL, just using for demo purposes
+        this.eventSource = new EventSource('/api/polls/' + this.id + '/events')
+
+        this.eventSource.addEventListener('poll-update', event => {
+          let data = JSON.parse(event.data)
+          this.poll = Object.assign({}, this.poll, data)
+        })
+      },
       async voteSubmit(opt) {
         try {
           const response = await ApiService.get('polls/' + this.id + '/vote/' + opt._id)
